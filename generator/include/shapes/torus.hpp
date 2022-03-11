69ee8189fae8c8288e20c91095f04d6e98166e17
Francisco Neves <62014132+franl08@users.noreply.github.com>
2022-03-11 20:16:37 +0000

    Added torus (#8)
    Co-authored-by: franl08 <franl08@github.com>, beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>

---
+++ generator/include/shapes/torus.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "shapes/primitive.hpp"
+
+class Torus : public Primitive {
+private:
+    float torus_radius;
+    float out_radius;
+    size_t n_slices;
+    size_t n_stacks;
+
+public:
+    Torus(int argc, char** argv);
+    std::vector<Point> calculateCoords() const;
+    ~Torus() {};
+};
