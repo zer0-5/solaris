c79e8d66556f267baa401418e728ce0725ff159b
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-03-12 19:21:48 +0000

    Optimization in curr and next

--- generator/src/shapes/sphere.cpp
+++ generator/src/shapes/sphere.cpp
@@ -17,13 +17,13 @@ std::vector<Point> Sphere::calculateCoords() const {
     float stacks_height = M_PI / n_stacks;
     float alpha = (2 * M_PI) / n_slices;
 
-    for (size_t stack = 0; stack < n_stacks; ++stack) {
-        float current_stack_beta = M_PI / 2 - stacks_height * stack;
-        float next_stack_beta = M_PI / 2 - stacks_height * (stack + 1);
+    float current_stack_beta = M_PI / 2;
+    for (size_t stack = 1; stack <= n_stacks; ++stack) {
+        float next_stack_beta = M_PI / 2 - stacks_height * stack;
 
-        for (size_t slice = 0; slice < n_slices; ++slice) {
-            float current_slice_alpha = alpha * slice;
-            float next_slice_alpha = alpha * (slice + 1);
+        float current_slice_alpha = 0;
+        for (size_t slice = 1; slice <= n_slices; ++slice) {
+            float next_slice_alpha = alpha * slice;
             // P3 --- P4
             // |      |
             // P1 --- P2
@@ -32,18 +32,22 @@ std::vector<Point> Sphere::calculateCoords() const {
             Point p3 = Point::spherical(radius, current_slice_alpha, next_stack_beta);
             Point p4 = Point::spherical(radius, next_slice_alpha, next_stack_beta);
 
-            if (stack != n_stacks - 1) {
+            if (stack != n_stacks) {
                 coords.push_back(p1);
                 coords.push_back(p3);
                 coords.push_back(p4);
             }
 
-            if (stack != 0) {
+            if (stack != 1) {
                 coords.push_back(p2);
                 coords.push_back(p1);
                 coords.push_back(p4);
             }
+
+            current_slice_alpha = next_slice_alpha;
         }
+
+        current_stack_beta = next_stack_beta;
     }
     return coords;
 }
